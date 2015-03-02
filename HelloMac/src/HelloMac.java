08cd8e4985a811baefd1abd84390b852213bd043
ljx213101212 <lijixiang.nus@gmail.com>
2015-03-02 14:43:39 +0800

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	HelloMac/src/HelloMac.java

--- HelloMac/src/HelloMac.java
+++ HelloMac/src/HelloMac.java
@@ -3,7 +3,11 @@ public class HelloMac {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+<<<<<<< HEAD
           System.out.println("Good afternoon!");
+=======
+          System.out.println("Good day!");
+>>>>>>> origin/master
 	}
 
 }
